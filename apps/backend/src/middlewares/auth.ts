efcd709fe3ea415cddef9625b137df3a39ff40ae
ezzabuzaid <ezza@kortext.com>
2025-11-03 14:40:56 +0300

    increase mem limits

--- apps/backend/src/middlewares/auth.ts
+++ apps/backend/src/middlewares/auth.ts
@@ -14,7 +14,6 @@ export const auth = betterAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       accessType: 'offline', // get a refresh token first try
       prompt: 'select_account consent',
-      redirectURI: `${process.env.BETTER_AUTH_URL}/api/auth/callback/google`,
       scope: [
         'https://www.googleapis.com/auth/tasks', // or tasks.readonly if you only need read
       ],
